ef14336b21d94c09dfb3d7fc77369ae79b07de63
Mouthwatering777 <bluesatellite777@gmail.com>
2022-12-08 07:02:51 +0100

    fixed urql endpoint bug

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -1,9 +1,7 @@
 import { createClient } from "urql";
 
 const client = createClient({
-  url: process.env.API_UR
-    ? process.env.API_UR
-    : "https://api.dev.anyaa.io/graphql",
+  url: "https://api.dev.anyaa.io/graphql",
   fetchOptions: () => {
     const token = localStorage.getItem("token");
     return {
