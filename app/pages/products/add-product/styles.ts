7b5006926e07e8b0268014e7132a9322f50e6f2d
Mouthwatering777 <bluesatellite777@gmail.com>
2022-12-11 14:21:06 +0100

    edit and delete product functionality added

--- app/pages/products/add-product/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-export const AddProductWrapper = styled.div`
-  .ant-input-number,
-  .ant-picker {
-    width: 100%;
-  }
-`;
--- app/pages/products/products-list/index.tsx
+++ app/pages/products/products-list/index.tsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
-import { GetProductsAction } from "~/redux/app/actions/product";
+import {
+  GetProductsAction,
+  DeleteProductAction,
+} from "~/redux/app/actions/product";
 import { useAppDispatch, useAppSelector } from "~/hooks/Store";
-import { Table, Button } from "antd";
+import { Table, Button, Popconfirm } from "antd";
 import { data as StateData, loading as StateLoading } from "~/redux/app";
-import { productTableColumns } from "./ProductsList.utils";
+
 import Drawer from "~/components/shared/drawer";
 import AddNewProduct from "../add-product";
+import type { ProductType } from "~/types/products";
+import type { ColumnsType } from "antd/es/table";
+import moment from "moment";
+import { ActionButtonsWrapper } from "../styles";
+
+import { DeleteFilled, EditFilled } from "@ant-design/icons";
 
 export default function Index() {
   const [productDrawerOpen, setProductDrawerOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedAction, setSelectedAction] = useState("");
   const dispatch = useAppDispatch();
   const data = useAppSelector(StateData);
   const loading = useAppSelector(StateLoading);
@@ -16,13 +27,83 @@ export default function Index() {
     dispatch(GetProductsAction());
   }, [dispatch]);
 
+  const productTableColumns: ColumnsType<ProductType> = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      responsive: ["md", "xs"],
+    },
+    {
+      title: "Arabic Title",
+      dataIndex: "title_ar",
+      key: "title_ar",
+      responsive: ["md", "xs"],
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      responsive: ["md", "xs"],
+    },
+    {
+      title: "Arabic Description",
+      dataIndex: "description_ar",
+      key: "description_ar",
+      responsive: ["md", "xs"],
+    },
+
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      responsive: ["md", "xs"],
+    },
+    {
+      title: "Created At",
+      render(value: string) {
+        return <div>{moment(value).format("DD-MM-YYYY")}</div>;
+      },
+      responsive: ["sm"],
+    },
+    {
+      title: "Actions",
+      render: (_: any, record: any) => {
+        return (
+          <ActionButtonsWrapper>
+            <EditFilled
+              className="warning-icon"
+              onClick={() => {
+                setSelectedProduct(record);
+                setProductDrawerOpen(true);
+                setSelectedAction("edit-product");
+              }}
+            />
+            <Popconfirm
+              title="Are you sure to delete this product?"
+              onConfirm={() => dispatch(DeleteProductAction(record.id))}
+              onCancel={() => console.log("cancel")}
+              okText="Yes"
+              cancelText="No"
+            >
+              <DeleteFilled className="danger-icon" />
+            </Popconfirm>
+          </ActionButtonsWrapper>
+        );
+      },
+    },
+  ];
+
   return (
     <>
       <div className="flex justify-end">
         <Button
           type="primary"
           className="mb-4"
-          onClick={() => setProductDrawerOpen(true)}
+          onClick={() => {
+            setProductDrawerOpen(true);
+            setSelectedAction("new-product");
+          }}
         >
           Create product
         </Button>
@@ -33,15 +114,18 @@ export default function Index() {
         loading={loading}
       />
       <Drawer
-        title="Add product"
+        title={
+          selectedAction === "new-product" ? "Add product" : "Edit product"
+        }
         size="large"
         open={productDrawerOpen}
         onClose={() => setProductDrawerOpen(false)}
         placement="right"
       >
         <AddNewProduct
-          selectedProduct={null}
+          selectedProduct={selectedProduct}
           setProductDrawerOpen={setProductDrawerOpen}
+          selectedAction={selectedAction}
         />
       </Drawer>
     </>
