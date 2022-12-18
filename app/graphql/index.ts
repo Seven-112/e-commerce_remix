f2480c7ed14d867363410b6e44b69401dadb1e42
Mouthwatering777 <bluesatellite777@gmail.com>
2022-12-18 16:43:29 +0100

    reverted base URL

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -2,7 +2,7 @@ import { createClient } from "urql";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 const client = createClient({
-  url: "http://localhost:3001/graphql",
+  url: "https://api.dev.anyaa.io/graphql",
   fetchOptions: () => {
     const token = cookies.get("accessToken");
     return {
