02411651b1239bc7b8a8558402f43a4ebc2efe49
Mouthwatering777 <bluesatellite777@gmail.com>
2022-12-20 07:44:15 +0100

    removed comments

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -7,7 +7,7 @@ import { resolve } from "node:path";
 import { renderToString } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import i18next from "./i18next.server";
-import i18n from "./i18n"; // your i18n configuration file
+import i18n from "./i18n";
 
 export default async function handleRequest(
   request: Request,
@@ -15,29 +15,24 @@ export default async function handleRequest(
   headers: Headers,
   context: EntryContext
 ) {
-  // First, we create a new instance of i18next so every request will have a
-  // completely unique instance and not share any state
   let instance = createInstance();
   const sheet = new ServerStyleSheet();
-  // Then we could detect locale from the request
+
   let lng = await i18next.getLocale(request);
-  // And here we detect what namespaces the routes about to render want to use
   let ns = i18next.getRouteNamespaces(context);
 
   await instance
-    .use(initReactI18next) // Tell our instance to use react-i18next
-    .use(Backend) // Setup our backend
+    .use(initReactI18next)
+    .use(Backend)
     .init({
-      ...i18n, // spread the configuration
-      lng, // The locale we detected above
-      ns, // The namespaces the routes about to render wants to use
+      ...i18n,
+      lng,
+      ns,
       backend: {
         loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json"),
       },
     });
 
-  // Then you can render your app wrapped in the I18nextProvider as in the
-  // entry.client file
   let markup = renderToString(
     <I18nextProvider i18n={instance}>
       <RemixServer context={context} url={request.url} />
